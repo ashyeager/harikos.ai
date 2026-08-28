6d673002e701614a08755f570345e9aa3434b313
ash <ashyeagerhq@gmail.com>
2026-08-28 17:56:23 +0400

    Deleted .env.example

--- packages/core/dist/truth-engine.d.ts
+++
@@ -1,8 +0,0 @@
-import { type CandidateClaim, type ProjectChange, type ProjectTruthClaim, type TruthContradiction } from "./domain.js";
-export interface TruthResolution {
-    truths: ProjectTruthClaim[];
-    contradictions: TruthContradiction[];
-    changes: ProjectChange[];
-}
-export declare function resolveTruth(previousTruths: ProjectTruthClaim[], candidates: CandidateClaim[], at?: string, commitSha?: string): TruthResolution;
-//# sourceMappingURL=truth-engine.d.ts.map
